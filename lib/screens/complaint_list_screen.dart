3086db52f45ff1709bd8648be8a8cae3187ff233
Ved Bhensdadia <137387473+vedbhensdadia@users.noreply.github.com>
2026-05-19 14:05:42 +0530

    okkk

--- lib/screens/complaint_list_screen.dart
+++ lib/screens/complaint_list_screen.dart
@@ -27,7 +27,7 @@ class _ComplaintListScreenState extends State<ComplaintListScreen> {
 
                 const SizedBox(width: 10),
 
-                const Text("All Complains"),
+                const Text("All Complain"),
               ],
             ),
 
