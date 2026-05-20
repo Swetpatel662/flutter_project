b993603e491441091b4bd47037789947d307441f
Swetpatel662 <patelswet975@gmail.com>
2026-05-20 15:04:31 +0530

    commit2

--- lib/screens/complaint_list_screen.dart
+++ lib/screens/complaint_list_screen.dart
@@ -96,7 +96,7 @@ class _ComplaintListScreenState extends State<ComplaintListScreen> {
                     });
                   },
 
-                  child: const Text("All"),
+                  child: const Text("all"),
                 ),
 
                 ElevatedButton(
